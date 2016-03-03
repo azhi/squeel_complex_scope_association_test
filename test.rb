f3f0e303f06e48782b53dca43b33192dac5f7907
azhi <astzhe@gmail.com>
2016-03-04 00:28:21 +0300

    fix complex association test
    it was failing even without squeel
    the reason for this was missing flag in child model: scope association was
    querying for the child models with flag set to true, but test bench was
    creating a child model with flag set to false

--- test.rb
+++ test.rb
@@ -6,7 +6,7 @@ require 'minitest/autorun'
 class BugTest < Minitest::Test
   def test_complex_scope_inside_association
     parent = Parent.create!
-    model = Model.create!(parent: parent, field: 'test1')
+    model = Model.create!(parent: parent, flag: true, field: 'test1')
     assert_equal 'test1', Parent.all.joins(:complex_scoped_models).first.models.first.field
   end
 end
